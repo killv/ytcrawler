70187422a2757c5cf065da95a8949332da0fba25
Alexey Ushakov <75662211+rr-alexey-ushakov@users.noreply.github.com>
2024-10-30 16:57:28 +0700

    see full url

--- youtube_crawler.v
+++ youtube_crawler.v
@@ -58,7 +58,7 @@ fn get_page(url string) string {
 	config := http.FetchConfig{
 		user_agent: 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0'
 	}
-	println('|   get page')
+	println('|   get page: ${url}')
 
 	resp := http.fetch(http.FetchConfig{ ...config, url: url }) or {
 		fetch_err := 'failed to fetch data from the server'
